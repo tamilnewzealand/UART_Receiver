537630e669e316cc6e2ca2ec7356fe6748e4c44b
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2016-10-18 22:00:01 +1300

    Second attempt at fixing Sylvain's commenting

--- Group_16.vhd
+++ Group_16.vhd
@@ -1,21 +1,21 @@
 -- ELECTENG 209 UART_ Receiver
 -- Last Edit: 2016/08/12
-/*
-    Copyright (C) 2016 Sylvain Bechet <sbec582@aucklanduni.ac.nz>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-        
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-        
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+--
+-- Copyright (C) 2016 Sylvain Bechet <sbec582@aucklanduni.ac.nz>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- any later version.
+--  
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--  
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
 
 -------------------------------------------------------
 -----------            S Counter            -----------
